b2f0a22621c3104fd2bc39c908abed25ed2f60b9
gutivanian <gutivanaliefs@gmail.com>
2025-10-15 21:50:50 +0700

    done

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -76,7 +76,7 @@ interface MenuItem {
 type UserRole = 'admin' | 'teacher' | 'student';
 
 // Menu Data
-const menuData: MenuItem[] = [
+const menuDataEng: MenuItem[] = [
   {
     icon: LayoutDashboard,
     text: 'Dashboard',
@@ -89,6 +89,133 @@ const menuData: MenuItem[] = [
     path: '/panel/users',
     roles: ['admin'],
     subItems: [
+      { icon: Users, text: 'Students', path: '/panel/users/students', roles: ['admin'] },
+      { icon: UserPlus, text: 'Edit Student', path: '/panel/users/students/edit', roles: ['admin'] },
+      { icon: UserCheck, text: 'Teachers', path: '/panel/users/teachers', roles: ['admin'] },
+      { icon: UserCog, text: 'Edit Teacher', path: '/panel/users/teachers/edit', roles: ['admin'] },
+      { icon: Shield, text: 'Admins', path: '/panel/users/admins', roles: ['admin'] },
+      { icon: UserCog, text: 'Update Admin', path: '/panel/users/admins/edit', roles: ['admin'] },
+      { icon: Shield, text: 'Roles', path: '/panel/users/roles', roles: ['admin'] }
+    ]
+  },
+  {
+    icon: GraduationCap,
+    text: 'Courses',
+    path: '/panel/courses',
+    roles: ['admin', 'teacher', 'student'],
+    subItems: [
+      { icon: School, text: 'Classes', path: '/panel/courses/dashboard', roles: ['admin', 'teacher'] },
+      { icon: GraduationCap, text: 'Edit Classes', path: '/panel/courses/classes-page', roles: ['admin','teacher','student','Student'] },
+      { icon: BookOpen, text: 'Courses', path: '/panel/courses/list', roles: ['admin', 'teacher', 'student'] },
+      { icon: Edit, text: 'Edit Courses', path: '/panel/courses/courses-page', roles: ['admin', 'teacher', 'student'] },
+      { icon: FileText, text: 'Request Courses', path: '/panel/courses/requests', roles: ['admin', 'teacher', 'student'] },
+      { icon: BarChart, text: 'Progress', path: '/panel/courses/progress', roles: ['admin', 'teacher', 'student'] },
+      { icon: Tags, text: 'Course Categories', path: '/panel/courses/categories', roles: ['admin'] },
+      { icon: FileBox, text: 'Packages', path: '/panel/courses/packages', roles: ['admin', 'student'] },
+      { icon: MessageCircle, text: 'Discussion', path: '/panel/courses/discussion', roles: ['admin', 'teacher', 'student'] },
+      { icon: HelpCircle, text: 'Q&A', path: '/panel/courses/qa', roles: ['admin', 'teacher', 'student'] },
+      { icon: MessageCircle, text: 'Reviews', path: '/panel/courses/reviews', roles: ['admin', 'teacher', 'student'] },
+      { icon: Award, text: 'Certificates', path: '/panel/courses/certificates', roles: ['admin', 'teacher', 'student'] }
+    ]
+  },
+  {
+    icon: FileQuestion,
+    text: 'Exam',
+    path: '/panel/exam',
+    roles: ['admin', 'teacher', 'student'],
+    subItems: [
+      { icon: FileText, text: 'Exams', path: '/panel/exam/dashboard', roles: ['admin', 'teacher', 'student'] },
+      { icon: FileEdit, text: 'Exam Schedules', path: '/panel/exam/exam-schedules', roles: ['admin', 'teacher'] },
+      { icon: PenTool, text: 'Questions', path: '/panel/exam/question-dashboard', roles: ['admin', 'teacher'] },
+      { icon: Edit, text: 'Edit Question', path: '/panel/exam/questions', roles: ['admin', 'teacher'] },
+      { icon: FolderEdit, text: 'Exam Categories', path: '/panel/exam/categories', roles: ['admin'] },
+      { icon: Tags, text: 'Question Categories', path: '/panel/exam/question-categories', roles: ['admin'] },
+      { icon: Trophy, text: 'Ranking', path: '/panel/exam/ranking', roles: ['admin', 'teacher', 'student'] },
+      { icon: Flag, text: 'Progress', path: '/panel/exam/progress', roles: ['admin', 'teacher', 'student'] }
+    ]
+  },
+  {
+    icon: Newspaper,
+    text: 'Blog',
+    path: '/panel/blog',
+    roles: ['admin', 'teacher'],
+    subItems: [
+      { icon: FileText, text: 'Contents', path: '/panel/blog/contents', roles: ['admin', 'teacher'] },
+      { icon: Tags, text: 'Categories', path: '/panel/blog/categories', roles: ['admin'] },
+      { icon: FileEdit, text: 'Add Contents', path: '/panel/blog/contents/add', roles: ['admin', 'teacher'] },
+      { icon: FileBox, text: 'Drafts', path: '/panel/blog/drafts', roles: ['admin', 'teacher'] },
+      { icon: MessageCircle, text: 'Comments', path: '/panel/blog/comments', roles: ['admin', 'teacher'] },
+      { icon: BarChart, text: 'Activity', path: '/panel/blog/activity', roles: ['admin'] },
+      { icon: Tag, text: 'SEO', path: '/panel/blog/seo', roles: ['admin'] },
+      { icon: DollarSign, text: 'Ads', path: '/panel/blog/ads', roles: ['admin'] }
+    ]
+  },
+  {
+    icon: CreditCard,
+    text: 'Payment',
+    path: '/panel/payment',
+    roles: ['admin'],
+    subItems: [
+      { icon: DollarSign, text: 'Revenue', path: '/panel/payment/revenue', roles: ['admin'] },
+      { icon: Receipt, text: 'Transactions', path: '/panel/payment/transactions', roles: ['admin'] },
+      { icon: CreditCard, text: 'Payment Methods', path: '/panel/payment/methods', roles: ['admin'] },
+      { icon: Percent, text: 'Taxes', path: '/panel/payment/taxes', roles: ['admin'] },
+      { icon: Tag, text: 'Discounts', path: '/panel/payment/discounts', roles: ['admin'] },
+      { icon: DollarSign, text: 'Commisions', path: '/panel/payment/commisions', roles: ['admin'] },
+      { icon: RefreshCcw, text: 'Refunds', path: '/panel/payment/refunds', roles: ['admin'] },
+      { icon: Wallet, text: 'Salaries', path: '/panel/payment/salaries', roles: ['admin'] },
+      { icon: Building, text: 'Operational', path: '/panel/payment/operational', roles: ['admin'] },
+      { icon: Tag, text: 'Promotion', path: '/panel/payment/promotion', roles: ['admin'] },
+      { icon: PartyPopper, text: 'Events', path: '/panel/payment/events', roles: ['admin'] }
+    ]
+  },
+  {
+    icon: Calendar,
+    text: 'Calendar',
+    path: '/panel/calendar',
+    roles: ['admin', 'teacher', 'student'],
+    subItems: [
+      { icon: CalendarDays, text: 'Calendar', path: '/panel/calendar/schedules', roles: ['admin', 'teacher', 'student'] },
+      { icon: Users, text: 'Classes', path: '/panel/calendar/classes', roles: ['admin', 'teacher', 'student'] },
+      { icon: FileQuestion, text: 'Exams', path: '/panel/calendar/exams', roles: ['admin', 'teacher', 'student'] },
+      { icon: ListTodo, text: 'Tasks', path: '/panel/calendar/tasks', roles: ['admin', 'teacher', 'student'] },
+      { icon: CalendarCheck, text: 'Events', path: '/panel/calendar/events', roles: ['admin', 'teacher', 'student'] },
+      { icon: CalendarClock, text: 'Promotion', path: '/panel/calendar/promotion', roles: ['admin'] }
+    ]
+  },
+  {
+    icon: MessageSquare,
+    text: 'Messages',
+    path: '/panel/messages',
+    roles: ['admin', 'teacher', 'student'],
+    subItems: [
+      { icon: MessageSquare, text: 'Messages In', path: '/panel/messages/inbox', roles: ['admin', 'teacher', 'student'] },
+      { icon: Share2, text: 'Broadcast', path: '/panel/messages/broadcast', roles: ['admin'] },
+      { icon: FileText, text: 'Templates', path: '/panel/messages/templates', roles: ['admin'] },
+      { icon: Mail, text: 'Email', path: '/panel/messages/email', roles: ['admin', 'teacher', 'student'] },
+      { icon: Bell, text: 'Notification', path: '/panel/messages/notifications', roles: ['admin', 'teacher', 'student'] },
+      { icon: Phone, text: 'Phone', path: '/panel/messages/phone', roles: ['admin', 'teacher', 'student'] },
+      { icon: Share2, text: 'Social Media', path: '/panel/messages/social', roles: ['admin'] },
+      { icon: TicketCheck, text: 'Support Tickets', path: '/panel/messages/support', roles: ['admin', 'teacher', 'student'] }
+    ]
+  }
+];
+const menuData: MenuItem[] = [
+  {
+    icon: LayoutDashboard,
+    text: 'Dashboard',
+    path: '/panel',
+    roles: ['admin', 'teacher', 'student']
+  },
+  {
+    icon: Users,
+    text: 'Akun',
+    path: '/panel/users',
+    roles: ['admin', 'student'],
+    subItems: [
+      { icon: Users, text: 'Students', path: '/panel/users/students', roles: ['admin'] },
+      { icon: Users, text: 'Students', path: '/panel/users/students', roles: ['admin'] },
+      { icon: Users, text: 'Students', path: '/panel/users/students', roles: ['admin'] },
       { icon: Users, text: 'Students', path: '/panel/users/students', roles: ['admin'] },
       { icon: UserPlus, text: 'Edit Student', path: '/panel/users/students/edit', roles: ['admin'] },
       { icon: UserCheck, text: 'Teachers', path: '/panel/users/teachers', roles: ['admin'] },
